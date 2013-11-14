aa4274d02499ba6e5b9cbbca111c118414532419
unisgn <unisgn@gmail.com>
2013-11-14 18:01:50 +0800

    m

--- sink/src/main/java/com/sink/service/core/FinancialAccountService.java
+++ sink/src/main/java/com/sink/service/core/FinancialAccountService.java
@@ -1,6 +1,12 @@
 package com.sink.service.core;
 
 public interface FinancialAccountService {
+    public List<FinancialAccount> findAllAccounts();
+    public List<FinancialAccount> searchAccount(String keyword);
 
-
+    public int addAccount(FinancialAccount account);
+    public void activateAccount(int id);
+    public void deactivateAccount(int id);
+    public void saveOrUpdateAccount(FinancialAccount account);
+    
 }
